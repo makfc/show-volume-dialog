4ca50d767fcffbff0a90f2287369ed5e3b316370
makfc <makfc1234@gmail.com>
2020-09-03 04:43:05 +0800

    chore: add apk

--- app/src/main/java/com/makfc/show_volume_dialog/BootUpReceiver.kt
+++ app/src/main/java/com/makfc/show_volume_dialog/BootUpReceiver.kt
@@ -8,12 +8,12 @@ class BootUpReceiver : BroadcastReceiver() {
     @SuppressLint("UnsafeProtectedBroadcastReceiver")
     override fun onReceive(context: Context, intent: Intent) {
         val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-        if (prefs.getBoolean("show_volume_percentage", true)) {
+        if (prefs.getBoolean("show_volume_percentage", false)) {
             val filter = IntentFilter()
             filter.addAction(SettingsActivity.VOLUME_CHANGED_ACTION)
-            context.registerReceiver(SettingsActivity.receiver, filter)
+            context.applicationContext.registerReceiver(SettingsActivity.receiver, filter)
         } else {
-            context.unregisterReceiver(SettingsActivity.receiver);
+            context.applicationContext.unregisterReceiver(SettingsActivity.receiver)
         }
     }
 }
